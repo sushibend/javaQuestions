7ba2b78e913f42df44d1beac762edd6ec5a3ebbf
sushibend <cheeeyman78@gmail.com>
2018-07-10 11:31:07 -0500

    preparing linkedlist

--- chapter2/src/Main.java
+++ chapter2/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
+
       //2.1
+        LinkedList<Integer> tempList = new LinkedList<Integer>();
+        tempList.add(1);
+        tempList.add(2);
+        tempList.add(3);
+        tempList.add(2);
+        tempList.add(4);
+
+        removeDuplicates(tempList);
     }
 
+    public static void removeDuplicates(LinkedList<Integer> tempList){
+        for (int bob : tempList) {
+            System.out.println(bob);
+        }
 }
+    }
